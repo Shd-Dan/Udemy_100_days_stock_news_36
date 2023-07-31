135e8e23b67fb9c1ecf1b6e1f831964a4d22fad2
“ShD <bolatuly.shaken@gmail.com>
2023-08-01 02:13:22 +0600

    commit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import datetime
 
 import requests
-import datetime
 
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
@@ -19,15 +18,13 @@ NEWS_API = "d1dd4b726bd740518b1c2321d8293cc9"
 
 
 # Optional: Format the SMS message like this:
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
+"""TSLA: 🔺2% Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. Brief: We at Insider Monkey have 
+gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings 
+show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash. 
+or "TSLA: 🔻5% Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. Brief: We at Insider Monkey 
+have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F 
+filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus 
+market crash."""
 
 """ -------------------------------------------- My Solution -------------------------------------------- """
 
@@ -36,7 +33,7 @@ response = requests.get(url)
 data = response.json()
 
 current_date = datetime.date.today()
-one_day = datetime.timedelta(days=3)
+one_day = datetime.timedelta(days=4)
 yesterday = current_date - one_day
 pre_yesterday = yesterday - datetime.timedelta(days=1)
 
@@ -44,17 +41,21 @@ yesterday_open = float(data['Time Series (Daily)'][str(yesterday)]["1. open"])
 pre_yesterday_close = float(data['Time Series (Daily)'][str(pre_yesterday)]["4. close"])
 price_percentage_diff = (((yesterday_open / pre_yesterday_close) * 100) - 100).__round__(2)
 
-# if price_percentage_diff > 1:
-#     print("Get News")
+if price_percentage_diff > 1:
+
+    news_url = (f"https://newsapi.org/v2/everything?"
+                f"q=tesla&"
+                f"from={str(yesterday)}&"
+                
+                f"sortBy=publishedAt&"
+                f"language = en&"
+                f"apiKey={NEWS_API}"
+                )
+    news_response = requests.get(news_url)
+
+    first_articles = news_response.json()["articles"][:3]
+    for article in first_articles:
+        print(f"Title: {article['title']}\n Description: {article['description']}")
 
 # print(
-#     f"Open yesterday {yesterday_open}\n Close pre-yesterday {pre_yesterday_close}\n Price change: {price_percentage_diff}%")
-
-
-news_url = (f"https://newsapi.org/v2/everything?"
-       f"q=tesla&"
-       f"from={str(yesterday)}&"
-       f"sortBy=publishedAt&"
-       f"apiKey={NEWS_API}")
-news_response = requests.get(news_url)
-print(news_response.json()["articles"][0]['title'])
+# f"Open yesterday {yesterday_open}\n Close pre-yesterday {pre_yesterday_close}\n Price change: {price_percentage_diff}%")
